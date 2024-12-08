e97f720514b50453a5b4d8fac42be59e18501a83
artemgon <artemhoncharuk123@gmail.com>
2024-12-08 15:36:30 +0200

    Task 1 is done.

--- classes.h
+++ classes.h
@@ -1,1 +1,59 @@
 #include "libs.h"
+
+class Circle
+{
+protected:
+	double radius;
+public:
+	Circle(double r = 0) : radius(r) {}
+	double getRadius() const { return radius; }
+	void setRadius(double r) { radius = r; }
+	double getArea() const { return 3.14159 * radius * radius; }
+	double getDiameter() const { return 2 * radius; }
+	double getCircumference() const { return 2 * 3.14159 * radius; }
+	virtual void printFunc() const
+	{
+		cout << "Circle: " << endl;
+		cout << "Radius: " << radius << endl;
+		cout << "Diameter: " << getDiameter() << endl;
+		cout << "Circumference: " << getCircumference() << endl;
+		cout << "Area: " << getArea() << endl;
+		cout << endl;
+	}
+	~Circle() = default;
+};
+
+class Square
+{
+protected:
+	double side;
+public:
+	Square(double s = 0) : side(s) {}
+	double getSide() const { return side; }
+	void setSide(double s) { side = s; }
+	double getArea() const { return side * side; }
+	double getPerimeter() const { return 4 * side; }
+	virtual void printFunc() const
+	{
+		cout << "Square: " << endl;
+		cout << "Side: " << side << endl;
+		cout << "Area: " << getArea() << endl;
+		cout << "Perimeter: " << getPerimeter() << endl;
+		cout << endl;
+	}
+	~Square() = default;
+};
+
+class InscridedCircle : public Circle, public Square
+{
+public:
+	InscridedCircle(double r) : Circle(r), Square(2 * r) {}
+	void printFunc() const override
+	{
+		cout << "Inscrided Circle: " << endl;
+		cout << endl;
+		Circle::printFunc();
+		Square::printFunc();
+	}
+	~InscridedCircle() = default;
+};
--- main.cpp
+++ main.cpp
@@ -2,5 +2,15 @@
 
 int main()
 {
+	Circle c(5);
+	c.printFunc();
+	Square s(5);
+	s.printFunc();
+	c.setRadius(10);
+	c.printFunc();
+	s.setSide(20);
+	s.printFunc();
+	InscridedCircle ic(10);
+	ic.printFunc();
 	return 0;
 }
